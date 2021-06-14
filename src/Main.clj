032223052f4e89be56b65bdcc79d9a8494b5b3dd
Ljingx <ljx20132200022@gmail.com>
2021-06-14 11:30:49 +0800

    * fix: c7

--- src/Main.clj
+++ src/Main.clj
@@ -1,5 +1,14 @@
 (ns Main)
-(defn hello-world [username]
-  (println (format "hello, %s" username)))
 
-(hello-world "ljx, cool")
+(def vowel? (set "aeiou"))
+
+(defn pig-latin [word]
+  (let [first-letter (first word)]
+    (if (vowel? first-letter)
+      (str word "ay")
+      (str (subs word 1) first-letter "ay")
+      ))
+  )
+
+(println (pig-latin "red"))
+(println (pig-latin "orange"))
