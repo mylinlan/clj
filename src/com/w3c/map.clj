91f4884bccb1526e042f250949090bc00262a4ed
Ljingx <ljx20132200022@gmail.com>
2021-06-27 13:52:05 +0800

    * fix: map

--- src/com/w3c/map.clj
+++ src/com/w3c/map.clj
@@ -3,3 +3,14 @@
 (def m {:a :b :c "123"})
 (println m)
 (println (:a m))
+(println (:b m))
+(println (contains? m :b))
+(println (contains? m :a))
+(println (= (:a m) :b))
+(println (m :a))
+
+;lookup by value
+(def hm {:foo "bar"})
+(println
+  (keep #(when (= (val %) "bar")
+           (key %)) hm))
